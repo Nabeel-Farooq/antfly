6af238b8976fe48cf8a05332a01c193b482411db
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-09 14:25:39 +0500

    Update main.go

--- cmd/antfly/main.go
+++ cmd/antfly/main.go
@@ -13,63 +13,134 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package main
 
 import (
 	"io"
+	"log"
+	"os"
 	"runtime"
+	"runtime/debug"
 
 	"github.com/antflydb/antfly/cmd/antfly/cmd"
 	"github.com/antflydb/antfly/lib/utils"
+
 	json "github.com/antflydb/antfly/pkg/libaf/json"
+
 	gojson "github.com/goccy/go-json"
 )
 
+const (
+	enableMutexProfiling = false
+	enableBlockProfiling = false
+)
+
+var (
+	// Set by GoReleaser via ldflags.
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func init() {
-	// Configure the JSON wrapper to use goccy/go-json for performance
+	configureJSON()
+	configureRuntime()
+	configureVersionMetadata()
+}
+
+func main() {
+	if err := execute(); err != nil {
+		log.Printf("Fatal error: %v", err)
+		os.Exit(1)
+	}
+}
+
+func execute() error {
+	cmd.Execute()
+	return nil
+}
+
+func configureJSON() {
 	json.SetConfig(json.Config{
 		Marshal: gojson.Marshal,
-		MarshalIndent: func(v any, prefix, indent string) ([]byte, error) {
-			return gojson.MarshalIndent(v, prefix, indent)
+
+		MarshalIndent: func(
+			v any,
+			prefix string,
+			indent string,
+		) ([]byte, error) {
+			return gojson.MarshalIndent(
+				v,
+				prefix,
+				indent,
+			)
 		},
+
 		Unmarshal: gojson.Unmarshal,
-		MarshalString: func(v any) (string, error) {
-			data, err := gojson.Marshal(v)
-			if err != nil {
-				return "", err
-			}
-			return string(data), nil
-		},
-		UnmarshalString: func(s string, v any) error {
+
+		MarshalString: marshalString,
+
+		UnmarshalString: func(
+			s string,
+			v any,
+		) error {
 			return gojson.Unmarshal([]byte(s), v)
 		},
+
 		NewEncoder: func(w io.Writer) json.Encoder {
-			return gojson.NewEncoder(w)
+			encoder := gojson.NewEncoder(w)
+
+			encoder.SetEscapeHTML(false)
+
+			return encoder
 		},
+
 		NewDecoder: func(r io.Reader) json.Decoder {
 			return gojson.NewDecoder(r)
 		},
 	})
 }
 
-// https://goreleaser.com/cookbooks/using-main.version/
-//
-// By default, GoReleaser will set the following 3 ldflags:
-//
-// main.version: Current Git tag (the v prefix is stripped) or the name of the snapshot, if you're using the --snapshot flag
-var version = "dev"
+func marshalString(v any) (string, error) {
+	data, err := gojson.Marshal(v)
+	if err != nil {
+		return "", err
+	}
 
-// main.commit: Current git commit SHA
-// commit = "none"
-// main.date: Date in the RFC3339 format
-// date = "unknown"
+	return bytesToString(data), nil
+}
 
-func main() {
-	runtime.SetMutexProfileFraction(1) // Enable mutex profiling
-	runtime.SetBlockProfileRate(1)     // Sample every blocking event
-	// Or, for a rate of 1 sample per second of blocking:
-	// runtime.SetBlockProfileRate(int(time.Second.Nanoseconds()))
+func configureRuntime() {
+	// Improve GC behavior for large workloads.
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	// Return memory to OS more aggressively.
+	debug.SetGCPercent(100)
+
+	if enableMutexProfiling {
+		runtime.SetMutexProfileFraction(1)
+	}
+
+	if enableBlockProfiling {
+		runtime.SetBlockProfileRate(1)
+	}
+}
+
+func configureVersionMetadata() {
 	cmd.Version = version
 	utils.Version = version
-	cmd.Execute()
+
+	log.Printf(
+		"Starting Antfly version=%s commit=%s date=%s",
+		version,
+		commit,
+		date,
+	)
+}
+
+// bytesToString avoids an unnecessary allocation.
+// Use carefully: the byte slice must not be modified after conversion.
+func bytesToString(b []byte) string {
+	return unsafeString(b)
 }
